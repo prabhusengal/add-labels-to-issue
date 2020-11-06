58b006b24c21c6332eb23b9acdc0f1bc04d8a5bd
Prabhu Sengal <prabhu.sengal@gmail.com>
2020-11-06 16:58:19 +0800

    Parse issues

--- dist/index.js
+++ dist/index.js
@@ -3,39 +3,37 @@ async function run() {
   try {
     // Fetch all the inputs
     const repository = core.getInput('repository');
-    const issuenumbers = core.getInput('issue_numbers');
     const labels = core.getInput('labels').split("\n").filter((x) => x !== "");
     const token = core.getInput('token');
-    if (issuenumbers != 'current') {
-      // Split the input 'repository' (format {owner}/{repo}) to be {owner} and {repo}
-      const splitRepository = repository.split('/');
-      if (splitRepository.length !== 2 || !splitRepository[0] || !splitRepository[1]) {
-        throw new Error(`Invalid repository '${repository}'. Expected format {owner}/{repo}.`);
-      }
+    var issuenumbers = []
+    // Split the input 'repository' (format {owner}/{repo}) to be {owner} and {repo}
+    const splitRepository = repository.split('/');
+    if (splitRepository.length !== 2 || !splitRepository[0] || !splitRepository[1]) {
+      throw new Error(`Invalid repository '${repository}'. Expected format {owner}/{repo}.`);
+    }
 
-      const repo_owner = splitRepository[0];
-      const repo_name = splitRepository[1];
+    const repo_owner = splitRepository[0];
+    const repo_name = splitRepository[1];
 
-      const github = require('@actions/github');
+    const github = require('@actions/github');
 
-      console.log(issuenumbers)
-      console.log(labels)
-      console.log(repo_owner)
-      console.log(repo_name)
-      console.log(JSON.stringify(github))
-      const title = github.context.payload.head_commit.message
-      console.log(title)
+    console.log(issuenumbers)
+    console.log(labels)
+    console.log(repo_owner)
+    console.log(repo_name)
+    console.log(JSON.stringify(github))
+    const title = github.context.payload.head_commit.message
+    console.log(title)
 
-      if (title && title.toLowerCase().indexOf("issues: ")) {
-        var issuesStartIndex = title.toLowerCase().indexOf("issues: ") + 8;
-        var issuesSubStr = title.substring(issuesStartIndex);
-        var issues = issuesSubStr.split(",");
-        var issue_nums = issues.map(a => {
-          return parseInt(a.trim())
-        })
-        console.log(issue_nums)
-        issuenumbers = issue_nums
-      }
+    if (title && title.toLowerCase().indexOf("issues: ")) {
+      var issuesStartIndex = title.toLowerCase().indexOf("issues: ") + 8;
+      var issuesSubStr = title.substring(issuesStartIndex);
+      var issues = issuesSubStr.split(",");
+      var issue_nums = issues.map(a => {
+        return parseInt(a.trim())
+      })
+      console.log(issue_nums)
+      issuenumbers = issue_nums
 
       //Execute the API "Add labels to an issue", see 'https://octokit.github.io/rest.js/v18#issues-add-labels'
       const { Octokit } = require("@octokit/rest");
